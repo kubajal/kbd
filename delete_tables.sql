4ba7e54a695e9cdeeb1572a64e089807e74493f9
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2018-11-28 21:54:28 +0100

    modify tables deletion

--- delete_tables.sql
+++ delete_tables.sql
@@ -1,11 +1,5 @@
-BEGIN
-
-FOR c IN (SELECT table_name FROM user_tables) LOOP
-EXECUTE IMMEDIATE ('DROP TABLE "' || c.table_name || '" CASCADE CONSTRAINTS');
-END LOOP;
-
-FOR s IN (SELECT sequence_name FROM user_sequences) LOOP
-EXECUTE IMMEDIATE ('DROP SEQUENCE ' || s.sequence_name);
-END LOOP;
-
-END;
+drop table orders cascade constraints;
+drop table clients cascade constraints;
+drop table order_items cascade constraints;
+drop table products cascade constraints;
+drop table product_versions cascade constraints;
