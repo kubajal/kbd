3524f78f532000a10ebc7a5fcf825839e772627e
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2018-12-01 20:38:06 +0100

    created  directory for triggers definitions

---
+++ triggers/ORDER_ITEMS.sh
@@ -0,0 +1,21 @@
+
+sqlplus64 $bd_user/$bd_password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=ora1.elka.pw.edu.pl)(Port=1521))(CONNECT_DATA=(SID=iais)))
+
+create or replace trigger order_item_added
+before insert on order_items
+for each row
+declare
+    products_count number;
+    price number;
+    total number;
+begin
+    select pv.price into price
+        from product_versions pv
+        where pv.product_id = :new.product_id
+        and pv.version_id = :new.version_id;
+    total := price * :new.products_count;
+    update orders
+    set orders.order_value = orders.order_value+ total
+    where orders.order_id = :new.order_id;
+end;
+/
