05c09252ccaaa18097c17ee55d9c1652e052794b
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2018-12-02 21:41:43 +0100

    Update tests/adding_order.sh

--- tests/adding_order.sh
+++ tests/adding_order.sh
@@ -1,4 +1,4 @@
-result="`sqlplus64 $db_user/$db_password@ora1.elka.pw.edu.pl/iais @tests/adding_order.sql << 'exit'`"
+result="`sqlplus64 $db_user/$db_password@ora1.elka.pw.edu.pl/iais @tests/adding_order.sql`"
 check=`echo $result | grep "128.03"`
 if [ "$check" = "" ]; then
     echo "TEST FAILED: wrong order value"
