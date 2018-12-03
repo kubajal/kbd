d9b50862570c7cac984a29a1c22902f8ed9bf275
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2018-12-03 20:32:00 +0100

    Update tests/adding_order.sh

--- tests/adding_order.sh
+++ tests/adding_order.sh
@@ -1,15 +1,19 @@
 export result="`echo EXIT | sqlplus64 $db_user/$db_password@ora1.elka.pw.edu.pl/iais @tests/order_value.sql`"
 check=`echo $result | grep "128.03"`
 if [ "$check" = "" ]; then
-	    echo "FAILED: wrong order value"
+        echo "TEST FAILED: wrong order value"
+        echo "Log of the test:"
+        echo "$result";
 else
-        echo "TESTOK: order value test passed"
+        echo "TEST PASSEd: order value test passed"
 fi
 
 export result="`echo EXIT | sqlplus64 $db_user/$db_password@ora1.elka.pw.edu.pl/iais @tests/available.sql`"
 check=`echo $result | grep "1109"`
 if [ "$check" = "" ]; then
-	    echo "FAILED: wrong order value"
+        echo "TEST FAILED: wrong number of available products."
+        echo "Log of the test:"
+        echo "$result";
 else
-        echo "TESTOK: order value test passed"
+        echo "TEST PASSED: order value test passed"
 fi
