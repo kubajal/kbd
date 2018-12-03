4f66f0bea1c34b85708ab02e75a0ae37abc81f23
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2018-12-03 20:39:41 +0100

    Update triggers/ORDER_ITEMS.sql

--- triggers/ORDER_ITEMS.sql
+++ triggers/ORDER_ITEMS.sql
@@ -4,15 +4,24 @@ for each row
 declare
     products_count number;
     price number;
-    total number;
+    total_price number;
 begin
+
+-- 'order_value' column in ORDERS table
     select pv.price into price
         from product_versions pv
         where pv.product_id = :new.product_id
         and pv.version_id = :new.version_id;
-    total := price * :new.products_count;
+    total_price := price * :new.products_count;
     update orders
-    set orders.order_value = orders.order_value+ total
+    set orders.order_value = orders.order_value + total_price
     where orders.order_id = :new.order_id;
+
+-- 'available' column in PRODUCT_VERSIONS column
+    update product_versions
+    set product_versions.available = product_versions.available - :new.products_count
+    where product_versions.product_id = :new.product_id
+        and product_versions.version_id = :new.version_id;
+
 end;
 /
