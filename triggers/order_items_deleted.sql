758af985914ebd53add242f2db2dd9213fcf8e79
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2018-12-06 20:58:01 +0100

    Update triggers/order_items_deleted.sql, triggers/order_items_added.sql, .gitlab-ci.yml files Deleted triggers/ORDER_ITEMS.sql

---
+++ triggers/order_items_deleted.sql
@@ -0,0 +1,14 @@
+-- DELETE
+
+before delete on order_items
+for each row
+begin
+
+-- 'available' column in PRODUCT_VERSIONS column
+    update product_versions
+    set product_versions.available = product_versions.available + :old.products_count
+    where product_versions.product_id = :old.product_id
+        and product_versions.version_id = :old.version_id;
+
+end;
+/
