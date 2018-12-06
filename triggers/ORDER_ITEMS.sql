eb2c149ccb2097dddf2b1b7dcebc617edc9bcdec
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2018-12-06 18:52:06 +0100

    Update ORDER_ITEMS.sql

--- triggers/ORDER_ITEMS.sql
+++ triggers/ORDER_ITEMS.sql
@@ -5,7 +5,19 @@ declare
     products_count number;
     price number;
     total_price number;
+    available number;
 begin
+
+    select pv.available into available
+        from product_versions pv
+        where pv.product_id = :new.product_id
+        and pv.version_id = :new.version_id;
+        
+    if(available - :new.products_count < 0)
+    then
+    RAISE_APPLICATION_ERROR( -20001, 
+                             'Not enough products available. Rolling back order.');
+    end if;
 
 -- 'order_value' column in ORDERS table
     select pv.price into price
