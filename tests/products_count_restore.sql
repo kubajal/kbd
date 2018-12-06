ff3723c43e277964c1293af9685a0dbfe4d5d9a8
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2018-12-06 20:38:03 +0100

    Update tests/adding_order.sh, tests/products_count_restore.sql files

--- tests/products_count_restore.sql
+++ tests/products_count_restore.sql
@@ -4,5 +4,9 @@ insert into product_versions values(1, 125, 'restore PRODUCTS_COUNT', 1.11, to_d
 /
 insert into order_items values(4, 1, 125, 6)
 /
-delete from order_items where product_id = 1 and version_id = 124 and order = 4
+delete from order_items where product_id = 1 and version_id = 125 and order_id = 4
+/
+select * from product_versions where product_id = 1
+    and version_id = 125
+    and available = 10;
 /
