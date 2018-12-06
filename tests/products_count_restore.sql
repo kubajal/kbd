e38235be8f5a95fe4bb1392b9950932b7e425797
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2018-12-06 20:45:30 +0100

    Update tests/adding_order.sh, tests/products_count_restore.sql files

--- tests/products_count_restore.sql
+++ tests/products_count_restore.sql
@@ -1,6 +1,6 @@
 insert into orders values(4, 6974, 0, to_date('2018-11-11 11:11:11', 'YYYY-MM-DD HH24:MI:SS'))
 /
-insert into product_versions values(1, 125, 'restore PRODUCTS_COUNT', 1.11, to_date('2018-11-11 11:11:11', 'YYYY-MM-DD HH24:MI:SS'), 10)
+insert into product_versions values(1, 125, 'restore PRODUCTS_COUNT', 1.11, to_date('2018-11-11 11:11:11', 'YYYY-MM-DD HH24:MI:SS'), 54321)
 /
 insert into order_items values(4, 1, 125, 6)
 /
@@ -8,5 +8,5 @@ delete from order_items where product_id = 1 and version_id = 125 and order_id =
 /
 select * from product_versions where product_id = 1
     and version_id = 125
-    and available = 10;
+    and available = 54321;
 /
