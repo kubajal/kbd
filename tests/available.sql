2a080719a069f8a5a4289bb56fa5566db784071c
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2018-12-23 18:50:44 +0100

    fixing output format

--- tests/available.sql
+++ tests/available.sql
@@ -1,3 +1,5 @@
+insert into clients(6974, 'first_name_6974', 'last_name_6974', '123456789', '1234', 'street_6974', 'city_6974', '12345')
+/
 insert into orders values(2, 6974, 0, to_date('2018-11-11 11:11:11', 'YYYY-MM-DD HH24:MI:SS'))
 /
 insert into product_versions values(1, 123, 'test available', 1.11, to_date('2018-11-11 11:11:11', 'YYYY-MM-DD HH24:MI:SS'), 1111)
