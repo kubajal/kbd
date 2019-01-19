726cf8a4dc198ea413674763b5075370087fde2a
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2019-01-19 13:39:19 +0100

    updated tests

--- tests/order_value.sql
+++ tests/order_value.sql
@@ -2,15 +2,15 @@ insert into clients values (2, 'first_name_2', 'last_name_2', '123456789', '1234
 /
 insert into orders values(2, 2, 0, to_date('2018-11-11 11:11:11', 'YYYY-MM-DD HH24:MI:SS'))
 /
-insert into order_items values(2, 1, 1, 2) -- 2 * 15,3   = 30,6
+insert into order_items values(2, 1, 1, 2) -- 2 * 5,26   = 10,52
 /
-insert into order_items values(2, 2, 1, 3) -- 3 * 9,93   = 29,79
+insert into order_items values(2, 2, 1, 3) -- 3 * 8,18   = 25,54
 /
-insert into order_items values(2, 3, 1, 4) -- 4 * 16,91  = 67,64 
+insert into order_items values(2, 3, 1, 4) -- 4 * 11,41  = 45,64
+-- ---------------------------------------------------- + ------
+--                                                          80,7
 /
 commit
 /
--- ----------------------------------------  + ----------------
---                                                         128,03
-select * from orders where order_id = 2 and order_value = 128.03;
+select * from orders where order_id = 2 and order_value = 80.7;
 /
