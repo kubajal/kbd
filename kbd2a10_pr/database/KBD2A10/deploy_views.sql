238a234dd26fad91c3a5eb1ee833e0dc7a1436dd
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2019-01-19 20:37:21 +0100

    added views creation

---
+++ kbd2a10_pr/database/KBD2A10/deploy_views.sql
@@ -0,0 +1,14 @@
+CREATE VIEW CLIENT_TRADE_BY_MONTH
+AS select first_name, last_name, trade, yr as "year", mnt as "month"
+  from clients left outer join 
+    (select client_id as cid, sum(order_value) as trade, extract(year from date_ordered) as yr, extract(month from date_ordered) as mnt
+        from orders
+        group by (client_id, extract(year from date_ordered), extract(month from date_ordered)))
+    on clients.client_id = cid;
+
+CREATE VIEW CURRENT_PRICES
+AS SELECT 
+    products.name, date_cr, tmp.ver
+FROM
+    products join (select max(date_created) as date_cr, max(version) as ver, product_id as prod_id from product_versions group by product_id) tmp
+    on products.product_id = prod_id;
