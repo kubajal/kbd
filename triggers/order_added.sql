0509eed322cf2bf95122d3169716fdb90998ae1a
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2019-01-19 19:12:24 +0100

    Update order_added.sql

--- triggers/order_added.sql
+++ triggers/order_added.sql
@@ -2,10 +2,8 @@ create or replace trigger order_added
 before insert on orders
 for each row
 begin
-
-    if(:new.order_value is null)
+    if :new.order_value is null
     then
-        :new.order_value0 := 0;
-    end if;                           
+        :new.order_value := 0;
+    end if;
 end;
-/
