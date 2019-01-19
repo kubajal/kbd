7838d26e3853041cf246acdf0bf6342447c2feca
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2019-01-19 19:23:34 +0100

    updated products_count_too_bug test case

--- tests/adding_order.sh
+++ tests/adding_order.sh
@@ -47,7 +47,7 @@ test_suite()
 {
         test "Inserting into ORDER_ITEMS should subtract from PRODUCT_VERSIONS.AVAILABLE" "@tests/available.sql" "1109"
         test "Inserting into ORDER_ITEMS should set proper ORDERS.ORDER_VALUE" "@tests/order_value.sql" "80.7"
-        test "Inserting into ORDER_ITEMS item with PRODUCT_COUNT bigger than ORDERS.AVAILABLE should throw error" "@tests/products_count_too_big.sql" "ORA-20001" # Not enough products available. Rolling back order.
+        test "Inserting into ORDER_ITEMS item with PRODUCT_COUNT bigger than ORDERS.AVAILABLE should throw error" "@tests/products_count_too_big.sql" "ORA-20002" # Not enough products available. Rolling back order.
         test "Deleting from ORDER_ITEMS should add to ORDERS.AVAILABLE" "@tests/products_count_restore.sql" "54321" # Adding to ORDERS.AVAILABLE when an order item is deleted.
 }
 
