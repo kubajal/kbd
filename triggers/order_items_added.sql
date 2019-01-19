cb1b9fcba8f45006e226623db13ac30a7618290b
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2019-01-19 17:30:48 +0100

    Update triggers/order_items_added.sql

--- triggers/order_items_added.sql
+++ triggers/order_items_added.sql
@@ -1,5 +1,3 @@
--- INSERT
-
 create or replace trigger order_item_added
 before insert on order_items
 for each row
@@ -8,16 +6,25 @@ declare
     price number;
     total_price number;
     available number;
+    does_product_version_exist number;
 begin
+
+    select count(*) into does_product_version_exist from product_versions where :new.product_id = product_id and :new.version = version;
+    
+    if(does_product_version_exist = 0)
+    then
+        RAISE_APPLICATION_ERROR( -20001,
+                               'Version of the product doesnt exist.');
+    end if;                           
 
     select pv.available into available
         from product_versions pv
         where pv.product_id = :new.product_id
         and pv.version = :new.version;
-        
+
     if(available - :new.products_count < 0)
     then
-    RAISE_APPLICATION_ERROR( -20001, 
+    RAISE_APPLICATION_ERROR( -20002,
                              'Not enough products available. Rolling back order.');
     end if;
 
@@ -38,4 +45,3 @@ begin
         and product_versions.version = :new.version;
 
 end;
-/
