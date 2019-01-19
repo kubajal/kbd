6cbee01cc731041a82c2e1840ff2d699cc050034
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2019-01-19 18:42:01 +0100

    Update feed_db.sql, delete_tables.sql files

--- delete_tables.sql
+++ delete_tables.sql
@@ -3,3 +3,7 @@ drop table clients cascade constraints;
 drop table order_items cascade constraints;
 drop table products cascade constraints;
 drop table product_versions cascade constraints;
+
+drop trigger order_added;
+drop trigger order_item_added;
+drop trigger order_item_deleted;
