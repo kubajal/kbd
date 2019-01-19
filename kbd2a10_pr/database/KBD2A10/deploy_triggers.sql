79ca2fb2d154a9e9e0833a8f88eba044acf78e84
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2019-01-19 22:11:05 +0100

    added triggers generated in JDeveloper

---
+++ kbd2a10_pr/database/KBD2A10/deploy_triggers.sql
@@ -0,0 +1,96 @@
+CREATE OR REPLACE TRIGGER AFTER_DELETE_ORDER_ITEM 
+AFTER DELETE ON ORDER_ITEMS 
+for each row
+BEGIN
+    -- lower the price of the order
+    select pv.price into unit_price
+        from product_versions pv
+        where pv.product_id = :old.product_id
+        and pv.version = :old.version;
+    price_sum := unit_price * :new.products_count;
+    update orders
+    set orders.order_value = orders.order_value - price_sum
+    where orders.order_id = :old.order_id;
+    
+    -- return products_count to the stock
+    update product_versions
+    set product_versions.available = product_versions.available + :old.products_count
+    where product_versions.product_id = :old.product_id
+        and product_versions.version = :old.version;
+END;
+/
+
+CREATE OR REPLACE TRIGGER AFTER_INSERT_ORDER_ITEM 
+AFTER INSERT ON ORDER_ITEMS 
+for each row
+declare
+    unit_price number;
+    price_sum number;
+    does_product_version_exist number;
+begin
+
+-- 'order_value' column in ORDERS table
+    select pv.price into unit_price
+        from product_versions pv
+        where pv.product_id = :new.product_id
+        and pv.version = :new.version;
+    price_sum := unit_price * :new.products_count;
+    update orders
+    set orders.order_value = orders.order_value + price_sum
+    where orders.order_id = :new.order_id;
+
+-- 'available' column in PRODUCT_VERSIONS column
+    update product_versions
+    set product_versions.available = product_versions.available - :new.products_count
+    where product_versions.product_id = :new.product_id
+        and product_versions.version = :new.version;
+end;
+/
+
+CREATE OR REPLACE TRIGGER BEFORE_INSERT_ORDER 
+BEFORE INSERT ON ORDERS 
+for each row
+begin
+    if :new.order_value is null
+    then
+        :new.order_value := 0;
+    end if;
+end;
+/
+
+CREATE OR REPLACE TRIGGER BEFORE_INSERT_ORDER_ITEM 
+BEFORE INSERT ON ORDER_ITEMS 
+for each row
+declare
+    available number;
+BEGIN
+    select pv.available into available
+        from product_versions pv
+        where pv.product_id = :new.product_id
+        and pv.version = :new.version;
+
+    if(available - :new.products_count < 0)
+    then
+    RAISE_APPLICATION_ERROR( -20002,
+                             'Not enough products available.');
+    end if;
+END;
+/
+
+CREATE OR REPLACE TRIGGER BEFORE_INSERT_PRODUCT_VERSION 
+BEFORE INSERT ON PRODUCT_VERSIONS 
+BEGIN
+    if :new.date_created is null
+    then
+      :new.date_created := sysdate;
+    end if;
+    if :new.available is null
+    then
+      :new.available := 0;
+    end if;
+    if :new.version_name is null
+    then
+      :new.version_name := '';
+    end if;
+END;
+/
--- kbd2a10_pr/database/KBD2A10/deploy_views.sql
+++ kbd2a10_pr/database/KBD2A10/deploy_views.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE VIEW CLIENT_TRADE_BY_MONTH
+SET SQLBLANKLINES ON
+CREATE VIEW CLIENT_TRADE_BY_MONTH
 AS select first_name, last_name, trade, yr as "year", mnt as "month"
   from clients left outer join 
     (select client_id as cid, sum(order_value) as trade, extract(year from date_ordered) as yr, extract(month from date_ordered) as mnt
@@ -6,9 +7,21 @@ AS select first_name, last_name, trade, yr as "year", mnt as "month"
         group by (client_id, extract(year from date_ordered), extract(month from date_ordered)))
     on clients.client_id = cid;
 
-CREATE OR REPLACE VIEW CURRENT_PRICES
+CREATE VIEW CURRENT_PRICES
 AS SELECT 
     products.name, date_cr, tmp.ver
 FROM
     products join (select max(date_created) as date_cr, max(version) as ver, product_id as prod_id from product_versions group by product_id) tmp
     on products.product_id = prod_id;
+
+CREATE VIEW HIGHEST_GAIN_PRODUCTS
+AS select max(gain) as max_gain, "year", "month", max(prod_id)
+from (select order_items.product_id as prod_id, sum(product_versions.price * order_items.products_count) as gain, extract(year from orders.date_ordered) as "year", extract(month from orders.date_ordered) as "month"
+    from order_items 
+        join product_versions on order_items.product_id = product_versions.product_id
+        join orders on order_items.order_id = orders.order_id
+    group by order_items.product_id, extract(year from orders.date_ordered), extract(month from orders.date_ordered))
+group by "year", "month"
+order by "year" desc, "month" desc;
+
+COMMENT ON TABLE HIGHEST_GAIN_PRODUCTS IS 'Accumulates';
