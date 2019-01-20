a6ffb66fa03e59cb086dc253ebf0bb946fec9bd1
Jakub Jałowiec <kuba.jal@users.noreply.github.com>
2019-01-20 23:55:37 +0100

    Update drop.sql

--- drop.sql
+++ drop.sql
@@ -4,6 +4,7 @@ drop trigger AFTER_DELETE_ORDER_ITEM;
 drop trigger AFTER_INSERT_ORDER_ITEM;
 drop trigger BEFORE_INSERT_ORDER;
 drop trigger BEFORE_INSERT_ORDER_ITEM;
+drop trigger BEFORE_INSERT_PRODUCT;
 drop trigger BEFORE_INSERT_PRODUCT_VERSION;
 
 drop view CLIENT_TRADE_BY_MONTH;
@@ -13,6 +14,8 @@ drop view HIGHEST_GAIN_PRODUCTS;
 drop sequence client_id_seq;
 drop sequence order_id_seq;
 drop sequence product_id_seq;
+
+drop procedure startWWW;
 
 drop table orders cascade constraints;
 drop table clients cascade constraints;
